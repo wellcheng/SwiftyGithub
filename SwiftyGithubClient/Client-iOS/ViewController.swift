75e9136cd528ea3eff912c72318ba5b6a3d460c5
wellcheng <326955439@qq.com>
2019-01-26 01:10:32 +0800

    update project

--- SwiftyGithubClient/Client-iOS/ViewController.swift
+++ SwiftyGithubClient/Client-iOS/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreText
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
